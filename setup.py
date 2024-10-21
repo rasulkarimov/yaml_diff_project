e4bbd9d40a06ce2adba2c936d110645c4da3da21
rkarimov <rkarimov@nymbus.com>
2024-10-21 17:13:42 +0500

    yaml_diff==0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yaml_diff',  
-    version='0.1', 
+    version='0.2', 
     description='A tool to compare YAML configuration files', 
     author='Rasul Karimov',
     author_email='rasul.karimov@gmail.com',
